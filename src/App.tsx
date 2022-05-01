1ca13f7ca1b5f3870c715e1c49bfe07944c68c73
Diego Garcia <diego.garciabrisa@maibornwolff.de>
2022-05-01 21:38:36 +0200

    feat(package): install react-router v6

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,25 @@
+import { Routes, Route, Link } from "react-router-dom"
+
+// Hooks
+import useFetchPokemonList from './hooks/useFetchPokemonList'
+
+// Custome hooks
+import Home from './pages/Home'
+import Pokemon from './pages/Pokemon'
+
 function App() {
+
+  const { data } = useFetchPokemonList()
+
+  console.log(data)
+
   return (
-    <div className="">
+
+    <div className="bg-blue h-full w-full">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="pokemon" element={<Pokemon />} />
+      </Routes>
     </div>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import { QueryClientProvider } from "react-query"
+import { queryClient } from "./services/reactQuery"
+import { BrowserRouter } from "react-router-dom"
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 )
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </BrowserRouter>
   </React.StrictMode>
 )
 
