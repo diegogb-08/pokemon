bf1bd135238ce46bca7967c8d2db47f784884d20
Diego Garcia Brisa <77103257+diegogb-08@users.noreply.github.com>
2022-05-03 21:28:25 +0200

    feat(motion): implement animation to grid and list

--- src/components/PokemonGrid.tsx
+++ src/components/PokemonGrid.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react"
 import { Pokemon } from "../types"
-
+import { motion } from "framer-motion"
 
 interface Props {
   pokemonList: Pokemon[] | undefined
@@ -11,10 +11,18 @@ const PokemonGrid: FC<Props> = ({ pokemonList }) => {
     <div className="h-full w-full grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-2">
       {
         pokemonList?.map(pokemon => {
-          return <div key={pokemon?.id} className="flex justify-center items-center flex-col p-2 bg-slate-300 rounded-md">
-            <img src={pokemon?.sprites.front_default} loading="lazy" />
-            <p className="font-bold">{pokemon?.name.toUpperCase()}</p>
-          </div>
+          return (
+            <motion.div
+              key={pokemon?.id}
+              className="flex justify-center items-center flex-col p-2 bg-slate-300 rounded-md hover:bg-slate-400 hover:cursor-pointer"
+              whileHover={{
+                scale: 1.02,
+                textShadow: "0px 0px 4px gray"
+              }}
+            >
+              <img src={pokemon?.sprites.front_default} loading="lazy" />
+              <p className="font-bold">{pokemon?.name.toUpperCase()}</p>
+            </motion.div>)
         })
       }
     </div>
--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react"
 import { Pokemon } from "../types"
-
+import { motion } from "framer-motion"
 
 interface Props {
   pokemonList: Pokemon[] | undefined
@@ -8,7 +8,24 @@ interface Props {
 
 const PokemonList: FC<Props> = ({ pokemonList }) => {
   return (
-    <div>PokemonList</div>
+    <div className="h-full w-full flex flex-col">
+      {
+        pokemonList?.map(pokemon => {
+          return (
+            <motion.div
+              key={pokemon?.id}
+              className="flex items-center flex-row p-2 mb-2 bg-slate-300 rounded-md hover:bg-slate-400 hover:cursor-pointer"
+              whileHover={{
+                scale: 1.01,
+                textShadow: "0px 0px 4px gray"
+              }}
+            >
+              <img src={pokemon?.sprites.front_default} loading="lazy" className="mx-8" />
+              <p className="font-bold">{pokemon?.name.toUpperCase()}</p>
+            </motion.div>)
+        })
+      }
+    </div>
   )
 }
 
