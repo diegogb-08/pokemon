d977a6d796a56b33285b91e1ddc3afcd84e52a12
Diego Garcia Brisa <77103257+diegogb-08@users.noreply.github.com>
2022-05-05 21:39:35 +0200

    feat(Home): add pagination

--- src/hooks/useFetchPokemonList.tsx
+++ src/hooks/useFetchPokemonList.tsx
@@ -1,10 +1,19 @@
+import { useMemo } from "react"
 import { useQuery } from "react-query"
+import { useSearchParams } from "react-router-dom"
 import { client } from '../services/api'
 import { Pokemon, PokemonList, PokemonListResult } from '../types'
 
 
 const useFetchPokemonList = () => {
-  return useQuery(['PokemonList'], () => client<PokemonList>(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=20`).then(async res => {
+  const [params, setParams] = useSearchParams()
+  const pageParam = params.get('page')
+  const pageNumber: number = useMemo(() => {
+    return pageParam ? parseInt(pageParam) : 1
+  }, [pageParam])
+  const offSet = (pageNumber * 20) - 20
+
+  return useQuery(['PokemonList', { pageNumber, offSet }], () => client<PokemonList>(`https://pokeapi.co/api/v2/pokemon?offset=${offSet}&limit=20`).then(async res => {
     try {
       if (res?.results) {
         return {
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,51 @@
-import { FC } from 'react'
-import { Outlet } from 'react-router'
+import { AnimatePresence } from 'framer-motion'
+import { ChangeEvent, FC, useEffect, useMemo, useState } from 'react'
+import { Outlet, useParams, useNavigate, useLocation } from 'react-router'
+import { Pagination } from '@mui/material'
+import { useSearchParams } from 'react-router-dom'
 
-const Home: FC = () => {
+interface Props {
+  totalItems: number | undefined
+}
+
+const Home: FC<Props> = ({ totalItems }) => {
+
+  const [params, setParams] = useSearchParams()
+  const pageParam = params.get('page')
+
+  const pageNumber: number = useMemo(() => {
+    return pageParam ? parseInt(pageParam) : 1
+  }, [pageParam])
+
+  const handleChangePage = (ev: ChangeEvent<unknown>, page: number) => {
+    setParams({
+      page: page.toString()
+    })
+  }
+
+
+  useEffect(() => {
+  }, [])
+
+  useEffect(() => {
+    if (!pageParam) {
+      setParams({ page: '1' })
+    }
+
+  }, [pageParam])
+
+
   return (
     <div className='h-full w-full p-2'>
-      <Outlet />
+      <AnimatePresence>
+        <Outlet />
+      </AnimatePresence>
+      {
+        totalItems &&
+        <div className='w-full flex justify-center mt-4'>
+          <Pagination onChange={handleChangePage} page={pageNumber} count={Math.ceil(totalItems / 20)} variant="outlined" color="primary" />
+        </div>
+      }
     </div>
   )
 }
